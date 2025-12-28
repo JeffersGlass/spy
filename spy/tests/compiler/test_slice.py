abb0333b9e6738ff111c49541e2564a24ad91576
Jeff Glass <glass.jeffrey@gmail.com>
2025-12-28 08:54:46 -0800

    More tests

--- spy/tests/compiler/test_slice.py
+++ spy/tests/compiler/test_slice.py
@@ -1,3 +1,5 @@
+import pytest
+
 from spy.fqn import FQN
 from spy.tests.support import CompilerTest, expect_errors, no_C, only_interp
 from spy.vm.b import B
@@ -14,3 +16,24 @@ class Test_Slice(CompilerTest):
             """)
         x = mod.foo()
         assert x == slice(1, 2, 3)
+
+    @only_interp
+    def test_list_slice_is_list(self):
+        mod = self.compile("""
+            def foo() -> list[i32]:
+                l = [1,2,3]
+                return l[0:2:1]
+            """)
+        x = mod.foo()
+        assert type(x) == list
+
+    @pytest.mark.xfail(reason="List slices currently return the whole list")
+    @only_interp
+    def test_list_slice(self):
+        mod = self.compile("""
+            def foo() -> list[i32]:
+                l = [1,2,3]
+                return l[0:2:1]
+            """)
+        x = mod.foo()
+        assert x == [1, 2]
