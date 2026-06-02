bbfab0f7c8cfc970b576b727b59a7994a3e682f6
Jeff Glass <glass.jeffrey@gmail.com>
2026-06-02 17:23:12 +0000

    initial work, needs str.__getitem__(slice)

--- spy/vm/str.py
+++ spy/vm/str.py
@@ -44,6 +44,7 @@ class W_Str(W_Object):
     __spy_storage_category__ = "value"
     __spy_lazy_attributes__ = {
         "isascii": FQN("_str::methods::isascii"),
+        "split": FQN("_str::methods::split"),
     }
 
     vm: "SPyVM"
